b887535834c75a7b47ffbb88665ecf29dc515620
kareem batisha <kareembatisha39@gmail.com>
2024-05-16 11:30:56 +0300

    complete the full sections and add animation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,25 @@
-
-export default function Footer() {
+import "./Footer.css";
+const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="flex">
+      <ul className="flex">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Speaking</a>
+        </li>
+        <li>
+          <a href="">Uses</a>
+        </li>
+      </ul>
+
+      <p>© 2023 Spencer Sharp. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,67 @@
+import Lottie from "lottie-react";
+import "./Hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 
-export default function Hero() {
+const Hero = () => {
+  const lottieRef = useRef();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./public\portfolio-modified.png"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Frontend Developer React.Js.
+        </motion.h1>
+
+        <p className="sub-title">
+          Junior React Frontend Developer with a passion for building responsive
+          web applications. ProficientinReact.js, HTML, CSS, and JavaScript.
+          Eager to learn and contribute in a collaborative
+          teamenvironment,dedicated to staying updated on industry best
+          practices and emerging technologies tocontinuouslyimprove skills and
+          deliver innovative solutions.
+        </p>
+
+        <div className="all-icons flex">
+          <div className="icon icon-twitter"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin"></div>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
